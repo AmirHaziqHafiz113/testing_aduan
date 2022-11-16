78a4d70e282073a06e0a34338b3cbeffd6283e2b
amirhh113 <finejake113@gmail.com>
2022-11-16 09:40:14 +0800

    added login function

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aduan Bernama</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="css/app.css'" rel='stylesheet'>
+    <script type="text/javascript">
+        var onloadCallback = function() {
+        };
+
+        var response = grecaptcha.getResponse();
+        if(response.length == 0) {
+            //reCaptcha not verified
+            alert("Please verify you are humann!");
+            return false;
+        }
+
+    </script>
+</head>
+<body>
+<div class="pr-content-wrapper">
+        <div class="pr-overlay"></div>
+        <div class="pr-content">
+
+            <header class="pr-navigation container">
+                <a href="" class="pr-logo">
+                    <div></div>
+                </a>
+                <nav class="pr-menu">
+                    <ul>
+                        <li><a href='login.php'>Admin</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <section class="pr-main pr-home-content container">
+                <h1 class="pr-fadein">Bernama Aduan</h1>
+                <p>Sila tekan butang di bawah untuk mengisi aduan anda ....</p>
+                <br>
+                <div class="page-content page-container" id="page-content">
+                    <div class="padding">
+                        <div class="row container d-flex justify-content-center">
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal-4" data-whatever="@fat">Isi Aduan</button>
+                                <div class="modal fade" id="exampleModal-4" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true" style="display: none;">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                            <h5 class="modal-title" id="ModalLabel">Butir Aduan</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            </div>
+                                            <div class="modal-body"><grammarly-extension style="position: absolute; top: 0px; left: 0px; pointer-events: none;"><div data-grammarly-part="highlights" style="position: absolute; top: 0px; left: 0px;"><div style="box-sizing: content-box; top: 1px; left: 1px; width: 0px; height: 0px; position: relative; pointer-events: none; overflow: hidden; border: 0px; border-radius: 0px; padding: 0px; margin: 0px;"><div style="position: absolute; top: 0px; left: 0px;"><div style="height: 815px; width: 1440px;"></div><div style="position: absolute; top: 0px; left: 0px; height: 815px; width: 1440px;"></div></div></div></div><div data-grammarly-part="button" style="position: absolute; top: 0px; left: 0px;"><div style="box-sizing: content-box; top: 1px; left: 1px; width: 0px; height: 0px; position: relative; pointer-events: none; overflow: hidden; border: 0px; border-radius: 0px; padding: 0px; margin: 0px;"><div style="position: absolute; transform: translate(-100%, -100%); top: -14px; left: -22px; pointer-events: all;"><div style="display: flex; flex-direction: row;"><div class="_3-ITD"><div class="_5WizN aN9_b _1QzSN"><div class="_3YmQx"><div title="Protected by Grammarly" class="_3QdKe">&nbsp;</div></div></div></div></div></div></div></div></grammarly-extension>
+                                            <form action='DataInsert' method='post' enctype='multipart/form-data'>
+                                                @csrf
+                                                <div class="form-group">
+                                                <label for="Nama_Pengadu" class="col-form-label">Nama Pengadu:</label>
+                                                    <input type="text" class="form-control" name="Nama_Pengadu" placeholder='Isi nama anda' required>
+                                                </div>
+                                                <div class="form-group">
+                                                <label for="Aduan_Info" class="col-form-label">Aduan:</label>
+                                                    <textarea class="form-control" id="form-aduan" name="Aduan_Info" placeholder='Isi aduan anda' required></textarea>
+                                                </div>
+                                                <div class="form-group">
+                                                <label for="No_Tel" class="col-form-label">Nombor Telefon:</label>
+                                                    <input type="tel" class="form-control" name="No_Tel" pattern="\+?6?(?:01[0-46-9]\d{7,8}|0\d{8})" placeholder='Isi nombor anda seperti: 0123456789' required>
+                                                </div>
+                                                <div class="form-group">
+                                                <label for="Email" class="col-form-label">Email:</label>
+                                                    <input type="email" class="form-control" name="Email" placeholder='Isi email anda' required>
+                                                </div>
+                                                <div class="g-recaptcha" id="my_captcha_form" data-sitekey="6LcAw7wiAAAAABwHXS4weO5EuyQEl5kjuC5tFDVr" required></div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="submit" class="btn btn-success">Send message</button>
+                                                    <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+            </section>
+            <div class="pr-arrow-wrapper">
+                <div class="pr-arrow"></div>
+            </div>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"async defer></script>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,161 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title>Dashtreme Admin - Free Dashboard for Bootstrap 4 by Codervent</title>
+  <!-- loader-->
+  <link href="assets/css/pace.min.css" rel="stylesheet"/>
+  <script src="assets/js/pace.min.js"></script>
+  <!--favicon-->
+  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+  <!-- Bootstrap core CSS-->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+  <!-- animate CSS-->
+  <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <!-- Icons CSS-->
+  <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+  <!-- Custom Style-->
+  <link href="assets/css/app-style.css" rel="stylesheet"/>
+  
+</head>
+
+<body class="bg-theme bg-theme1">
+
+<!-- start loader -->
+   <div id="pageloader-overlay" class="visible incoming"><div class="loader-wrapper-outer"><div class="loader-wrapper-inner" ><div class="loader"></div></div></div></div>
+   <!-- end loader -->
+
+<!-- Start wrapper-->
+ <div id="wrapper">
+
+ <div class="loader-wrapper"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>
+	<div class="card card-authentication1 mx-auto my-5">
+		<div class="card-body">
+		 <div class="card-content p-2">
+		  <div class="card-title text-uppercase text-center py-3">Sign In</div>
+		    <form>
+			  <div class="form-group">
+			  <label for="exampleInputUsername" class="sr-only" name="username">Username</label>
+			   <div class="position-relative has-icon-right">
+				  <input type="text" id="exampleInputUsername" class="form-control input-shadow" placeholder="Enter Username">
+				  <div class="form-control-position">
+					  <i class="icon-user"></i>
+				  </div>
+			   </div>
+			  </div>
+			  <div class="form-group">
+			  <label for="exampleInputPassword" class="sr-only" name="password">Password</label>
+			   <div class="position-relative has-icon-right">
+				  <input type="password" id="exampleInputPassword" class="form-control input-shadow" placeholder="Enter Password">
+				  <div class="form-control-position">
+					  <i class="icon-lock"></i>
+				  </div>
+			   </div>
+			  </div>
+			<div class="form-row">
+			 <div class="form-group col-6">
+			   <div class="icheck-material-white">
+                <input type="checkbox" id="user-checkbox" checked="" />
+                <label for="user-checkbox">Remember me</label>
+			  </div>
+			 </div>
+			 <div class="form-group col-6 text-right">
+			  <a href="reset-password.html">Reset Password</a>
+			 </div>
+			</div>
+			 <button type="button" class="btn btn-light btn-block" name="login-btn">Sign In</button>
+			</div>
+			 </form>
+		   </div>
+       <div class="card-footer text-center py-3">
+		    <p class="text-warning mb-0">Do not have an account? <a href="register.php"> Sign Up here</a></p>
+		  </div>
+		  </div>
+	  </div>
+    
+     <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+	
+	<!--start color switcher-->
+   <div class="right-sidebar">
+    <div class="switcher-icon">
+      <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
+    </div>
+    <div class="right-sidebar-content">
+
+      <p class="mb-0">Gaussion Texture</p>
+      <hr>
+      
+      <ul class="switcher">
+        <li id="theme1"></li>
+        <li id="theme2"></li>
+        <li id="theme3"></li>
+        <li id="theme4"></li>
+        <li id="theme5"></li>
+        <li id="theme6"></li>
+      </ul>
+
+      <p class="mb-0">Gradient Background</p>
+      <hr>
+      
+      <ul class="switcher">
+        <li id="theme7"></li>
+        <li id="theme8"></li>
+        <li id="theme9"></li>
+        <li id="theme10"></li>
+        <li id="theme11"></li>
+        <li id="theme12"></li>
+		<li id="theme13"></li>
+        <li id="theme14"></li>
+        <li id="theme15"></li>
+      </ul>
+      
+     </div>
+   </div>
+  <!--end color switcher-->
+	
+	</div><!--wrapper-->
+	<!--php script-->
+
+    <?php
+    $conn = mysqli_connect('localhost', 'root', '', 'aduan');
+  
+    if (isset($_POST['login-btn'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+  
+        $select = "SELECT * FROM user WHERE username='$U_Name' AND password='$U_Password'";
+        $run = mysqli_query($conn, $select);
+        $row_user = mysqli_fetch_array($run);
+  
+        $db_username = $row_user['U_Name'];
+        $db_password = $row_user['U_password'];
+        if ($username == $db_email && $password == $db_password) {
+            echo "<script>window.open('display_data.php','_self');</script>";
+            $_SESSION['username'] = $db_username;
+        } else {
+            echo "Email or Password is Wrong!";
+        }
+    }
+    ?>
+  
+    <!--end php script-->
+  <!-- Bootstrap core JavaScript-->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+	
+  <!-- sidebar-menu js -->
+  <script src="assets/js/sidebar-menu.js"></script>
+  
+  <!-- Custom scripts -->
+  <script src="assets/js/app-script.js"></script>
+  
+</body>
+</html>
